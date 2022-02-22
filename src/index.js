9dac06a39c489aa8f03a4373d4a93474ab51b0ab
kimdaeyoon <eodbszla@gmail.com>
2022-02-22 15:17:40 +0900

    Delete Todo

--- src/index.js
+++ src/index.js
@@ -6,13 +6,27 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addToDo = (text, id) => {
+  return {
+    type: ADD_TODO,
+    text,
+    id,
+  };
+};
+
+const deleteToDo = (id) => {
+  return { type: DELETE_TODO, id };
+};
+
 const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: action.id }];
+      return [{ text: action.text, id: action.id }, ...state];
     case DELETE_TODO:
-      return [];
+      //html에서 받아온 id는 string 타입이기에 숫자타입으로 변경해준다
+      const newArr = state.filter((el) => el.id !== parseInt(action.id));
+      return newArr;
     default:
       return state;
   }
@@ -22,28 +36,38 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchAddToDo = (text, id) => {
+  //dispatch로 reduce에게 데이터를 전달해줄 수 있다 --> action.type or action.text로 활용할 수 있다
+  store.dispatch(addToDo(text, id));
+};
+
+const dispatchDeleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDo(id));
+};
+
 const paintToDos = () => {
   const toDos = store.getState();
+  ul.innerText = "";
   toDos.forEach((toDo) => {
     const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
     li.id = toDo.id;
     li.innerText = toDo.text;
+    li.appendChild(btn);
     ul.appendChild(li);
   });
 };
 
 store.subscribe(paintToDos);
 
-const addToDo = (text, id) => {
-  //dispatch로 reduce에게 데이터를 전달해줄 수 있다 --> action.type or action.text로 활용할 수 있다
-  store.dispatch({ type: ADD_TODO, text, id });
-};
-
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  addToDo(toDo, Date.now());
+  dispatchAddToDo(toDo, Date.now());
 };
 
 form.addEventListener("submit", onSubmit);
