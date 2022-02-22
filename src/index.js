af70ef173614989663183afd5a2c8769b66a6b79
kimdaeyoon <eodbszla@gmail.com>
2022-02-22 14:57:10 +0900

    Add toDo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
+import { createStore } from "redux";
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = (toDo) => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: action.id }];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
+
+const addToDo = (text, id) => {
+  //dispatch로 reduce에게 데이터를 전달해줄 수 있다 --> action.type or action.text로 활용할 수 있다
+  store.dispatch({ type: ADD_TODO, text, id });
 };
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo);
+  addToDo(toDo, Date.now());
 };
 
 form.addEventListener("submit", onSubmit);
